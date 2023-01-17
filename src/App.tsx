76d4bd724214d19bf4eb74bebdb5c0612116697a
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-01-17 13:57:02 -0300

    Configuring Front-End

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles/global.css";
+import { Habit } from "./components/habit";
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={10} />
+    </div>
+  );
+}
+
+export default App;
