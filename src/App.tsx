accedda30af84bc9e8598d4f24143debde40c2e5
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-01-19 14:44:23 -0300

    Created component Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
+import { Header } from "./components/header";
 import "./styles/global.css";
-import Logo from "./assets/logo.svg";
-import { Plus } from "phosphor-react";
 
 export function App() {
   return (
     <div className=" w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-          <img src={Logo} alt="Habits" />
-          <button
-            type="button"
-            className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
-          >
-            <Plus size={20} className=" text-violet-500" />
-            Novo Hábito
-          </button>
-        </div>
+        <Header />
       </div>
     </div>
   );
