500b983800a1752da5f4c41fbeeb868df867aa3b
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-01-22 15:32:39 -0300

    Finilized four class

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 import { FormEvent, useState } from "react";
+import { api } from "../libs/axios";
 
 const availableWeekDays = [
   "Domingo",
@@ -15,9 +16,19 @@ const availableWeekDays = [
 export function NewHabitForm() {
   const [title, setTitle] = useState("");
   const [weekDays, setWeekDays] = useState<number[]>([]);
-  function createNewHabit(event: FormEvent) {
+  console.log("executou");
+  async function createNewHabit(event: FormEvent) {
     event.preventDefault();
-    console.log(title, weekDays);
+    if (!title || weekDays.length === 0) {
+      return;
+    }
+    await api.post("habits", {
+      title,
+      weekDays,
+    });
+    setTitle("");
+    setWeekDays([]);
+    alert("Hábito craido com sucesso");
   }
   function handleToggleWeekDay(weekDay: number) {
     if (weekDays.includes(weekDay)) {
@@ -38,6 +49,7 @@ export function NewHabitForm() {
         id="title"
         placeholder="ex.:Exercícios, dormir bem, etc..."
         autoFocus
+        value={title}
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         onChange={(event) => setTitle(event.target.value)} //toda vez que usuario escrever algo nesse imput sera anotado na variavel estado
       />
@@ -51,6 +63,7 @@ export function NewHabitForm() {
             <Checkbox.Root
               key={weekDay}
               className="flex items-center gap-3 group"
+              checked={weekDays.includes(index)}
               onCheckedChange={() => handleToggleWeekDay(index)}
             >
               <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
