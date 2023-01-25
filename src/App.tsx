70b27df1cef70439a24e130a29241fbd26909a61
Joao Antonio <joaoantoniomartinst@gmail.com>
2023-01-25 15:36:01 -0300

    Ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Header } from "./components/Header";
+// import { Header } from "./components/Header";
 import { SummaryTable } from "./components/SummaryTable";
 import "./styles/global.css";
 import "./libs/dayjs";
@@ -7,7 +7,7 @@ export function App() {
   return (
     <div className=" w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <Header />
+        {/* <Header /> */}
         <SummaryTable />
       </div>
     </div>
